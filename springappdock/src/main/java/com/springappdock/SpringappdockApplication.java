d895d6266245d2fd9cc7b8cd86ee069e36a6c9c9
hp <rnaman51@gmail.com>
2023-01-17 16:11:21 +0530

    spring boot app

---
+++ springappdock/src/main/java/com/springappdock/SpringappdockApplication.java
@@ -0,0 +1,13 @@
+package com.springappdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappdockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappdockApplication.class, args);
+	}
+
+}
---
+++ springappdock/src/test/java/com/springappdock/SpringappdockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springappdock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappdockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
